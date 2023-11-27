318a868be4a6964f5c5f46aa3c3e5fc0571ff6ae
jeremy <jeremy.adams.fisher@gmail.com>
2023-11-26 23:16:20 -0800

    :adhesive_bandage: Resample everything at 16kHz

--- sdvfinetune/data.py
+++ sdvfinetune/data.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import librosa
 import torch
 from einops import rearrange
 from pytorch_lightning import LightningDataModule
@@ -19,20 +19,24 @@ class VideoDataset(Dataset):
 
     def __getitem__(self, idx):
         fp = self.fps[idx]
-        video, audio, _ = read_video(str(fp), pts_unit="sec")
+        video, audio, info = read_video(str(fp), pts_unit="sec")
+        audio = librosa.resample(audio.numpy(), orig_sr=info["audio_fps"], target_sr=16_000)
         assert video.shape[1:] == (
             576,
             1024,
             3,
         ), f"{Path(fp).name} is incorrectly shaped: {video.shape}"
-        return video, audio
+        return video.float(), torch.from_numpy(audio)
 
 
 def collate_av(batch):
     videos, audios = zip(*batch)
 
     video = torch.nn.utils.rnn.pad_sequence(videos, padding_value=0.0)
-    video = rearrange(video, "t b h w c -> b t h w c")
+    video = rearrange(video, "t b h w c -> b c h w t")
+
+    # TODO: keep track of which frames are 0-padded
+    # video_mask = video.sum(dim=1) != 0.0
 
     audios = [rearrange(audio, "c t -> t c") for audio in audios]
     audio = torch.nn.utils.rnn.pad_sequence(audios, padding_value=0.0)
@@ -44,7 +48,7 @@ def collate_av(batch):
 class VideoDataModule(LightningDataModule):
     def __init__(self, data_dir, batch_size, num_workers):
         super().__init__()
-        self.data_dir = data_dir
+        self.data_dir = Path(data_dir)
         self.batch_size = batch_size
         self.num_workers = num_workers
 
